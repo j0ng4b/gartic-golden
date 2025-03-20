b5200537741b89947a841825896d2dce44fa646d
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-03-20 09:40:38 -0300

    feat: agora na execução é possivel executar o server ou client

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+import argparse
+import logging
+
+from game.config import Config
 from game.client import Client
+from game.server import Server
 
 
-def main():
-    client = Client()
-    client.start()
+def main(args):
+    if args.address is None or args.port is None:
+        logging.warning('Nenhum endereço ou porta definido, usando o do ambiente')
+
+        args.address = Config.SERVER_ADDRESS
+        args.port = Config.SERVER_PORT
+
+
+    mode = None
+    if args.server:
+        mode = Server(args.address, args.port)
+    else:
+        mode = Client(args.address, args.port)
+    mode.start()
+
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', '--server', action='store_true', default=False, help='run in server mode')
+    parser.add_argument('-a', '--address', type=str, default=None, help='set server address')
+    parser.add_argument('-p', '--port', type=int, default=None, help='set server port')
+
+    main(parser.parse_args())
 
