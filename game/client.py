ff82d994ba608f3ecefdea7c441c6dd47c6e7cd1
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-03-20 09:47:56 -0300

    feat: estrutara base de comunicação entre o cliente e o servidor

--- game/client.py
+++ game/client.py
@@ -1,28 +1,45 @@
+import logging
 import socket
-import threading
-
-from game.config import Config
 
 
 class Client:
-    def __init__(self):
-        if Config.SERVER_ADDRESS is None or Config.SERVER_PORT is None:
-            raise ValueError('SERVER_ADDRESS and SERVER_PORT must be set in the environment')
+    def __init__(self, address, port):
+        if address is None or port is None:
+            raise ValueError('server address and port must be set')
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.connect((Config.SERVER_ADDRESS, int(Config.SERVER_PORT)))
+        self.socket.connect((address, int(port)))
 
-        # Inicializa a thread que vai lidar com a comunicação
-        thread = threading.Thread(target=self.handle_communication)
-        thread.daemon = True
-        thread.start()
+        self.name = 'Player'
 
     def start(self):
-        ...
+        self.register()
 
     def handle_input(self):
         ...
 
-    def handle_communication(self):
+
+    ###
+    ### Communication methods
+    ###
+    def send_message(self, type, *args):
+        self.socket.sendall(f'{type}:{';'.join(args)}'.encode())
+        return self.socket.recv(1034).decode()
+
+    def register(self):
+        res = self.send_message('REGISTER', self.name)
+        if res != 'OK':
+            logging.error(res)
+
+    def create_room(self, room_type, room_name, room_password=None):
+        ...
+
+    def close_room(self, room_code):
+        ...
+
+    def list_rooms(self, room_type=None):
+        ...
+
+    def enter_room(self, room_code, room_password=None):
         ...
 
