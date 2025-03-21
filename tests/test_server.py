d87aa142f9a365685ee958d24d29160ff3fa4577
Jonatha Gabriel <60396154+j0ng4b@users.noreply.github.com>
2025-03-21 19:53:08 -0300

    test: incluído mais testes, mensagem `STATUS`

--- tests/test_server.py
+++ tests/test_server.py
@@ -63,12 +63,15 @@ def test_register_success(server):
     assert any(client['name'] == 'Alice' for client in server.clients)
 
 
-def test_room_invalid_args(server):
+def test_room_client_unregistred(server):
     addr = ('127.0.0.1', 5001)
 
     # Not registered
     response = server.parse_message('ROOM', ['pub', 'notRegistered'], addr)
     assert response == 'Cliente não registrado'
+
+def test_room_invalid_args(server):
+    addr = ('127.0.0.1', 5001)
 
     # Register a client to create a room
     server.parse_message('REGISTER', ['Bob'], addr)
@@ -165,8 +168,58 @@ def test_list_rooms(server):
     assert 'RoomTwo' in lines[0]
 
 
+def test_status_client_unregistred(server):
+    addr = ('127.0.0.1', 6001)
+
+    # Test when run the command of unregistred client
+    response = server.parse_message('STATUS', [], addr)
+    assert response == 'Cliente não registrado'
+
+
+def test_status_invalid_arguments(server):
+    addr = ('127.0.0.1', 6001)
+    server.parse_message('REGISTER', ['Big Smoke'], addr)
+    
+    # Test if accepting any argument
+    response = server.parse_message('STATUS', ['arg'], addr)
+    assert response == 'Número de argumentos inválido'
+
+    # Test when client isn't on any room
+    response = server.parse_message('STATUS', [], addr)
+    assert response == 'Cliente não está em nenhuma sala'
+
+
+def test_status_format(server):
+    addr = ('127.0.0.1', 6001)
+    server.parse_message('REGISTER', ['Pikachu'], addr)
+
+    room_code = '1'
+    server.rooms = [{
+         'name': 'TestRoom',
+         'code': room_code,
+         'password': None,
+         'max_clients': 10,
+         'clients': [addr]
+    }]
+
+    client = server.get_client(addr[0], addr[1])
+    server.clients[client]['room'] = room_code
+
+    response = server.parse_message('STATUS', [], addr)
+    assert response == f'pub,TestRoom,{room_code},1,10'
+
+
+def test_enter_client_unregistred(server):
+    addr = ('127.0.0.1', 6001)
+
+    # Test when run the command of unregistred client
+    response = server.parse_message('ENTER', ['1'], addr)
+    assert response == 'Cliente não registrado'
+
+
 def test_enter_invalid_arguments(server):
     addr = ('127.0.0.1', 6000)
+    server.parse_message('REGISTER', ['Carl Johnson'], addr)
 
     # Test when more than two arguments are provided.
     response = server.parse_message('ENTER', ['room_code', 'pwd', 'extra'], addr)
@@ -175,6 +228,7 @@ def test_enter_invalid_arguments(server):
 
 def test_enter_invalid_room_code(server):
     addr = ('127.0.0.1', 6001)
+    server.parse_message('REGISTER', ['Shrek'], addr)
 
     # Simulate the case when get_room returns None (room not found)
     response = server.parse_message('ENTER', ['999'], addr)
@@ -183,6 +237,7 @@ def test_enter_invalid_room_code(server):
 
 def test_enter_already_in_room(server):
     addr = ('127.0.0.1', 6002)
+    server.parse_message('REGISTER', ['Ash'], addr)
 
     room_code = '1'
     server.rooms = [{
