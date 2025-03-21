5ceb9500b97a209b6774ee8cbb5777ea5e4b2421
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-03-21 19:08:10 -0300

    fix: bloqueio na função send_message devido a handle_server_messages
    Como handle_server_messages ficava a todo momento esperando mensagens do
    servidor ele recebia mensagens que era esperadas como resposta do
    send_message como este não recebia resposta ele ficava travado.

--- game/client.py
+++ game/client.py
@@ -15,46 +15,66 @@ class BaseClient:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.connect((address, int(port)))
 
+        self.mutex = threading.Lock()
+        self.msgs = []
+
         self.name = 'Player'
 
         self.room = None
 
     def start(self):
-        self.register()
-
-        # Start the server thread
+        # Inicia o a comunicação com o servidor, deve ser inciado antes de
+        # qualquer coisa devido a pilha de mensagem
         server_thrd = threading.Thread(target=self.handle_server_messages)
         server_thrd.daemon = True
         server_thrd.start()
 
+        self.register()
+
     def handle_server_messages(self):
         while True:
+            # Essa função recebe toda e qualquer mensagem mesmo as que não são
+            # comandos do servidor
             msg = self.socket.recv(1024).decode()
 
-            # Parse message
-            if ':' not in msg:
-                logging.error('Mensagem inválida')
+            # Quando há um ':' na message essa é uma mensagem de comando vinda
+            # do servidor, faz análise da mensagem
+            if ':' in msg:
+                msg_type, args = msg.split(':')
+                args = args.split(';')
+                if len(args) == 1 and args[0] == '':
+                    args = []
+
+                response = self.parse_message(msg_type, args)
+                if response is not None:
+                    self.socket.sendall(response.encode())
+
                 continue
 
-            msg_type, args = msg.split(':')
-            args = args.split(';')
-            if len(args) == 1 and args[0] == '':
-                args = []
-
-            response = self.parse_message(msg_type, args)
-            if response is not None:
-                self.socket.sendall(response.encode())
+            # Quando não é um comando do servidor apenas joga a mensagem na
+            # pilha de mensagens para que seja analisada por outra parte do
+            # código
+            with self.mutex:
+                self.msgs.append(msg)
 
 
     def parse_message(self, msg_type, args, address=None):
         return 'Tipo de mensagem inválido'
 
     ###
-    ### Sever communication methods
+    ### Métodos de comunicação com o servidor
     ###
+    def get_message(self):
+        while True:
+            with self.mutex:
+                if len(self.msgs) == 0:
+                    continue
+
+                return self.msgs.pop(0)
+
     def send_message(self, type, *args):
-        self.socket.sendall(f'{type}:{';'.join(args)}'.encode())
-        return self.socket.recv(1034).decode()
+        self.socket.sendall(f"{type}:{';'.join(args)}".encode())
+        return self.get_message()
 
     def register(self):
         res = self.send_message('REGISTER', self.name)
@@ -180,6 +200,8 @@ class TextClient(BaseClient):
         else:
             print('Não foi possível criar a sala')
 
+        self.menu = 0
+
     def list_rooms_menu(self):
         print('Listar salas:')
         print('1. todas')
@@ -222,3 +244,4 @@ class TextClient(BaseClient):
     def enter_room_menu(self):
         # TODO: implementar entrar na sala
         self.menu = 0
+
