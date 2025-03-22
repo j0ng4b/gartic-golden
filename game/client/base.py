9149cab1e4f5fdddb1ce5aa4ce90c4f184c988d0
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-03-22 15:15:44 -0300

    refactor: simplificação do código

--- game/client/base.py
+++ game/client/base.py
@@ -13,43 +13,33 @@ class BaseClient:
             raise ValueError('server address and port must be set')
 
         self.name = 'Player'
-
         self.room = None
 
         # Contexto de execução, armazena informações de execução de cada thread
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        server_socket.connect((address, int(port)))
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.connect((address, int(port)))
 
-        self.context = {
-            # Contexto de execução da padrão
-            0: {
-                'socket': server_socket,
-                'mutex': threading.Lock(),
-                'msgs': [],
-            }
-        }
+        self.mutex = threading.Lock()
+        self.msgs = []
 
     def start(self):
         # Inicia o a comunicação com o servidor, deve ser inciado antes de
         # qualquer coisa devido a pilha de mensagem
-        server_thrd = threading.Thread(target=self.handle_messages, args=(self.parse_server_message,))
-        server_thrd.daemon = True
-        server_thrd.start()
+        thread = threading.Thread(target=self.handle_messages, args=(self.parse_message,))
+        thread.daemon = True
+        thread.start()
 
         self.server_register()
 
     def handle_messages(self, parser):
         while True:
-            # Quando não acha o contexto para a thread usa o contexto padrão, ou
-            # seja, essa é a thread de comunicação com o servidor
-            context = self.get_context()
-
             # Essa função recebe toda e qualquer mensagem mesmo as que não são
             # comandos do servidor
-            msg = context['socket'].recv(1024).decode()
+            msg, address = self.socket.recvfrom(1024)
 
             # Quando há um ':' na message essa é uma mensagem de comando vinda
             # do servidor, faz análise da mensagem
+            msg = msg.decode()
             if ':' in msg:
                 msg_type, args = msg.split(':')
                 args = args.split(';')
@@ -58,74 +48,36 @@ class BaseClient:
 
                 response = parser(msg_type, args)
                 if response is not None:
-                    context['socket'].sendall(response.encode())
+                    self.socket.sendto(response.encode(), address)
 
                 continue
 
             # Quando não é um comando do servidor apenas joga a mensagem na
             # pilha de mensagens para que seja analisada por outra parte do
             # código
-            with context['mutex']:
-                context['msgs'].append(msg)
+            with self.mutex:
+                self.msgs.append(msg)
 
-    def setup_client(self, address, port):
-        thread_id = threading.current_thread().ident
-        if thread_id is None:
-            raise Exception('No thread identifier')
+    def parse_message(self, msg_type, args):
+        # Mensagens enviadas pelo servidor
 
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        client_socket.connect((address, int(port)))
-
-        # Armazena informações do cliente conectados a sala
-        self.context[thread_id] = {
-            'socket': client_socket,
-            'mutex': threading.Lock(),
-            'msgs': [],
-
-            'name': None,
-        }
-
-        self.handle_messages(self.parser_client_message)
-
-    def parse_server_message(self, msg_type, args):
-        if msg_type == 'CONNECT':
-            client_thread = threading.Thread(target=self.setup_client, args=args)
-            client_thread.daemon = True
-            client_thread.start()
-        return None
-
-    def parser_client_message(self, msg_type, args):
+        # Mensagens enviadas por outros clientes
         return None
 
 
     ###
     ### Métodos principais para comunicação
     ###
-    def get_context(self):
-        context_id = threading.current_thread().ident
-        if context_id is None:
-            return self.context[0]
-
-        context = self.context.get(context_id)
-        if context is None:
-            context = self.context[0]
-
-        return context
-
     def get_message(self):
-        context = self.get_context()
-
         while True:
-            with context['mutex']:
-                if len(context['msgs']) == 0:
+            with self.mutex:
+                if len(self.msgs) == 0:
                     continue
 
-                return context['msgs'].pop(0)
+                return self.msgs.pop(0)
 
     def send_message(self, type, *args):
-        context = self.get_context()
-
-        context['socket'].send(f"{type}:{';'.join(args)}".encode())
+        self.socket.send(f"{type}:{';'.join(args)}".encode())
         return self.get_message()
 
 
