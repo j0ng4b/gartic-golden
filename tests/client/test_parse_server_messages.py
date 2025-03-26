433c67e7df70ea0255368872d52747e3cbfa5b9f
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-03-26 06:40:49 -0300

    test(client): correção nos test de connect e desconnect #14

--- tests/client/test_parse_server_messages.py
+++ tests/client/test_parse_server_messages.py
@@ -5,14 +5,16 @@ def test_parse_server_message_connect(client):
 
 
     client.parse_server_message('CONNECT', ['client2'])
-    assert client.msgs == []
     assert 'client2' in client.room_clients
+    assert client.msgs['client2'] == []
     assert client.room_clients['client2']['name'] == 'GREET_PLAYER'
 
 
 def test_parse_server_message_disconnect(client):
+    client.msgs['client2'] = []
     client.room_clients['client2'] = {'name': 'Test', 'msgs': []}
 
     client.parse_server_message('DISCONNECT', ['client2'])
     assert 'client2' not in client.room_clients
     assert len(client.room_clients) == 0
+
