652f026a0db63f75e0a081ce5a8daa2004507676
Jonatha Gabriel <60396154+j0ng4b@users.noreply.github.com>
2025-03-27 19:34:52 +0000

    test: adicionado teste para validar nome da sala

--- tests/server/test_room_message.py
+++ tests/server/test_room_message.py
@@ -1,53 +1,61 @@
 def test_error_on_client_unregistred(server):
-    addr = ('127.0.0.1', 5001)
+    addr = ('127.0.0.1', 6000)
 
     response = server.parse_server_message('ROOM', ['pub', 'notRegistered'], addr)
     assert response == 'Cliente não registrado'
 
 
 def test_complain_about_extra_arguments(server):
-    addr = ('127.0.0.1', 5001)
-    server.parse_server_message('REGISTER', ['Bob'], addr)
+    addr = ('127.0.0.1', 6000)
+    server.parse_server_message('REGISTER', ['Tester'], addr)
 
     response = server.parse_server_message('ROOM', ['pub'], addr)
     assert response == 'Número de argumentos inválido'
 
 
 def test_complain_about_missing_arguments(server):
-    addr = ('127.0.0.1', 5001)
-    server.parse_server_message('REGISTER', ['Bob'], addr)
+    addr = ('127.0.0.1', 6000)
+    server.parse_server_message('REGISTER', ['Tester'], addr)
 
     response = server.parse_server_message('ROOM', ['pub'], addr)
     assert response == 'Número de argumentos inválido'
 
 
 def test_invalid_room_type(server):
-    addr = ('127.0.0.1', 5001)
-    server.parse_server_message('REGISTER', ['Bob'], addr)
+    addr = ('127.0.0.1', 6000)
+    server.parse_server_message('REGISTER', ['Tester'], addr)
 
     response = server.parse_server_message('ROOM', ['invalid', 'RoomName'], addr)
     assert response == 'Tipo da sala inválido'
 
 
+def test_invalid_room_name(server):
+    addr = ('127.0.0.1', 6000)
+    server.parse_server_message('REGISTER', ['Tester'], addr)
+
+    response = server.parse_server_message('ROOM', ['pub', ''], addr)
+    assert response == 'Nome da sala inválido'
+
+
 def test_missing_password_for_private_room(server):
-    addr = ('127.0.0.1', 5001)
-    server.parse_server_message('REGISTER', ['Bob'], addr)
+    addr = ('127.0.0.1', 6000)
+    server.parse_server_message('REGISTER', ['Tester'], addr)
 
     response = server.parse_server_message('ROOM', ['priv', 'RoomName'], addr)
     assert response == 'Senha não fornecida para sala privada'
 
 
 def test_provided_password_for_public_room(server):
-    addr = ('127.0.0.1', 5001)
-    server.parse_server_message('REGISTER', ['Bob'], addr)
+    addr = ('127.0.0.1', 6000)
+    server.parse_server_message('REGISTER', ['Tester'], addr)
 
     response = server.parse_server_message('ROOM', ['pub', 'RoomName', 'extra'], addr)
     assert response == 'Sala pública não requer senha'
 
 
 def test_create_public_room(server):
-    addr = ('127.0.0.1', 5002)
-    server.parse_server_message('REGISTER', ['Carol'], addr)
+    addr = ('127.0.0.1', 6000)
+    server.parse_server_message('REGISTER', ['Tester'], addr)
 
     response = server.parse_server_message('ROOM', ['pub', 'CoolRoom'], addr)
     assert response == "1"
@@ -65,8 +73,8 @@ def test_create_public_room(server):
 
 
 def test_create_private_room(server):
-    addr = ('127.0.0.1', 5003)
-    server.parse_server_message('REGISTER', ['Dave'], addr)
+    addr = ('127.0.0.1', 6000)
+    server.parse_server_message('REGISTER', ['Tester'], addr)
 
     response = server.parse_server_message('ROOM', ['priv', 'SecretRoom', 'mypassword'], addr)
     assert response == "1"
