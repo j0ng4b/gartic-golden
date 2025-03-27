82169ceef51b624611809aafd429346c4a979f1f
Jonatha Gabriel <60396154+j0ng4b@users.noreply.github.com>
2025-03-27 18:39:40 +0000

    test: atualizado testes de test_server.py
    added: teste para verificar se o servidor está analizando todas mensagens
    que ele deve processar.
    
    added: teste para verificar se o servidor está reportando mensagens que
    não devem ser processadas.
    
    close #24

--- tests/server/test_server.py
+++ tests/server/test_server.py
@@ -1,15 +1,15 @@
 def test_get_invalid_client(server):
-    addr = ('127.0.0.1', 5008)
+    addr = ('127.0.0.1', 6000)
     assert server.get_client(addr[0], addr[1]) is None
 
 
 def test_get_valid_client(server):
-    addr = ('127.0.0.1', 5008)
-    server.parse_server_message('REGISTER', ['Grace'], addr)
+    addr = ('127.0.0.1', 6000)
+    server.parse_server_message('REGISTER', ['Tester'], addr)
 
     client = server.get_client(addr[0], addr[1])
     assert client is not None
-    assert client['name'] == 'Grace'
+    assert client['name'] == 'Tester'
 
 
 def test_get_invalid_room(server):
@@ -34,3 +34,34 @@ def test_get_valid_room(server):
     assert room['password'] is None
     assert room['max_clients'] == 10
 
+
+def test_parse_valid_messages_types(server):
+    addr = ('127.0.0.1', 6000)
+
+    test_table = [
+        'REGISTER',
+        'UNREGISTER',
+        'ROOM',
+        'CROOM',
+        'LIST',
+        'ENTER',
+        'LEAVE',
+        'END',
+    ]
+
+    for msg_type in test_table:
+        response = server.parse_server_message(msg_type, [], addr)
+        assert response != 'Tipo de mensagem inválido'
+
+
+def test_parse_invalid_messages_types(server):
+    addr = ('127.0.0.1', 6000)
+
+    response = server.parse_server_message('register', [], addr)
+    assert response == 'Tipo de mensagem inválido'
+
+    response = server.parse_server_message('Register', [], addr)
+    assert response == 'Tipo de mensagem inválido'
+
+    response = server.parse_server_message('Invalid', [], addr)
+    assert response == 'Tipo de mensagem inválido'
