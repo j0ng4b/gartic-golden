65e5ded1da80a602bcbba7cb9b499f9b1dc79c9e
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-03-28 11:27:50 -0300

    test(server): correção no test_end_message

--- tests/server/test_end_message.py
+++ tests/server/test_end_message.py
@@ -24,7 +24,7 @@ def test_client_not_on_any_room(server):
 def test_only_owner_can_remove_room(server):
     addr1 = ('127.0.0.1', 6000)
     server.parse_server_message('REGISTER', ['Tester1'], addr1)
-    server.parse_server_message('ROOM', ['pub', 'TestRoom'], addr1)
+    server.parse_server_message('ROOM', ['pub', 'TestRoom', 'Test'], addr1)
 
     addr2 = ('127.0.0.1', 6001)
     server.parse_server_message('REGISTER', ['Tester2'], addr2)
@@ -37,7 +37,7 @@ def test_only_owner_can_remove_room(server):
 def test_end_room(server):
     addr1 = ('127.0.0.1', 6000)
     server.parse_server_message('REGISTER', ['Tester1'], addr1)
-    server.parse_server_message('ROOM', ['pub', 'TestRoom'], addr1)
+    server.parse_server_message('ROOM', ['pub', 'TestRoom', 'Test'], addr1)
 
     addr2 = ('127.0.0.1', 6001)
     server.parse_server_message('REGISTER', ['Tester2'], addr2)
@@ -52,7 +52,7 @@ def test_end_room(server):
 def test_send_end_message_to_clients(server):
     addr1 = ('127.0.0.1', 6000)
     server.parse_server_message('REGISTER', ['Tester1'], addr1)
-    server.parse_server_message('ROOM', ['pub', 'TestRoom'], addr1)
+    server.parse_server_message('ROOM', ['pub', 'TestRoom', 'Test'], addr1)
 
     addr2 = ('127.0.0.1', 6001)
     server.parse_server_message('REGISTER', ['Tester2'], addr2)
@@ -67,7 +67,7 @@ def test_send_end_message_to_clients(server):
 def test_end_not_in_game_room(server):
     addr = ('127.0.0.1', 6000)
     server.parse_server_message('REGISTER', ['Tester'], addr)
-    server.parse_server_message('ROOM', ['pub', 'TestRoom'], addr)
+    server.parse_server_message('ROOM', ['pub', 'TestRoom', 'Test'], addr)
 
     response = server.parse_server_message('END', [], addr)
     assert response == 'A sala não está em partida'
