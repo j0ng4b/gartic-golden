499c1bb2d21d8b00b0393e15dd19c4bb08b56e98
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-03-29 17:50:05 -0300

    test(client): testes do cliente temporariamente removidos

--- tests/client/test_client_greet_messages.py
+++
@@ -1,6 +0,0 @@
-def test_parse_greet_message(client):
-    client.room_clients['client1'] = {'name': None, 'msgs': []}
-
-    result = client.parse_client_message('client2', 'GREET', [])
-    assert result == client.name
-
