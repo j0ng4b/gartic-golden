2b58ca1106958da2b04a80c43326ea1ca436812d
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-04-12 11:47:06 -0300

    feat: desenha a lista de salas

--- game/screen/pages.py
+++ game/screen/pages.py
@@ -172,6 +172,13 @@ class RoomsPage(BasePage):
         super().__init__()
 
         self.rooms = []
+        self.room_start_pos = (
+            Size.SCREEN_WIDTH // 2 - 330,
+            Size.SCREEN_HEIGHT // 2 - 30
+        )
+        self.room_rect = pygame.Rect(0, 0, 200, 80)
+
+        self.font = None
 
         # Tempo para atualizar a lista de salas automaticamente
         self.auto_list_time = 0
@@ -179,6 +186,8 @@ class RoomsPage(BasePage):
 
     def init(self, client, surface, resource, goto_page):
         super().init(client, surface, resource, goto_page)
+
+        self.font = resource.load_font('Acme-Regular', 19)
 
         self.add_components(
             components.Image(
@@ -213,10 +222,35 @@ class RoomsPage(BasePage):
             self.update_rooms_list()
             self.auto_list_time = current_time
 
+        # Verifica se o mouse está sobre uma sala
+        for i in range(len(self.rooms)):
+            room_rect = self.room_rect.move(
+                self.room_start_pos[0] + (i % 3) * 220,
+                self.room_start_pos[1] + (i // 3) * 100
+            )
+
+            if room_rect.collidepoint(pygame.mouse.get_pos()):
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+
     def draw(self):
+        if self.surface is None or self.font is None:
+            return
         super().draw()
 
-        # TODO: Desenhar a lista de salas
+        for i in range(len(self.rooms)):
+            room = self.rooms[i]
+
+            room_rect = self.room_rect.move(
+                self.room_start_pos[0] + (i % 3) * 220,
+                self.room_start_pos[1] + (i // 3) * 100
+            )
+            pygame.draw.rect(self.surface, Color.WHITE, room_rect, border_radius=10)
+
+            # Desenha o texto da sala
+            text = f"{room['name']} ({room['num_clients']}/{room['max_clients']})"
+            text_surface = self.font.render(text, True, Color.BLACK)
+            text_rect = text_surface.get_rect(center=room_rect.center)
+            self.surface.blit(text_surface, text_rect)
 
     def handle_input(self, event):
         super().handle_input(event)
@@ -248,8 +282,9 @@ class RoomsPage(BasePage):
                 'type': data[0],
                 'name': data[1],
                 'code': data[2],
-                'num_clients': data[3],
-                'max_clients': data[4]
+                'theme': data[3],
+                'num_clients': data[4],
+                'max_clients': data[5]
             })
 
 
